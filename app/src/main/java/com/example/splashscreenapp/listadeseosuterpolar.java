30b1faa37241f237b9ec6b015e802b9f3fbe14eb
Aprendiz <estivenb605@gmail.com>
2022-06-24 14:45:19 -0500

    listadeseo

--- app/src/main/java/com/example/splashscreenapp/listadeseosuterpolar.java
+++ app/src/main/java/com/example/splashscreenapp/listadeseosuterpolar.java
@@ -6,23 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class detallesueterpolar extends AppCompatActivity implements View.OnClickListener{
+public class listadeseosuterpolar extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detallesueterpolar);
+        setContentView(R.layout.activity_listadeseosuterpolar);
     }
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.imageView8:
+            case R.id.imageView13:
                 Intent intent = new Intent(this, usuario_cliente_catalogo.class);
                 startActivity(intent);
-                break;
-            case R.id.imageView13:
-                Intent intent1 = new Intent(this, listadeseosuterpolar.class);
-                startActivity(intent1);
                 break;
 
         }
